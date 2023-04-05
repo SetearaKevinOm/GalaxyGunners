bd791cac924306e5668115e14ea99403d9abbcc1
Bryce Sandall <86949903+bsandall2@users.noreply.github.com>
2023-04-05 17:23:07 +1000

    UI Update, New Shoot SFX

---
+++ Assets/Team Members/Bryce/VoiceOver/VisualizerScript.cs
@@ -0,0 +1,45 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class VisualizerScript : MonoBehaviour
+{
+    public float minHeight;
+    public float maxHeight;
+    public float updateSenstivity;
+    [Space(15)]
+    public RectTransform[] visualizerObjects;
+    [Space(15)]
+    public AudioClip track;
+    public bool loop;
+    [Space(15)]
+    public int visualizerSimples;
+
+    AudioSource _audioSource;
+
+    void Start()
+    {
+        visualizerObjects = GetComponentsInChildren<RectTransform>();
+
+        if (!track)
+            return;
+
+        _audioSource = new GameObject("AudioSource").AddComponent<AudioSource>();
+        _audioSource.loop = loop;
+        _audioSource.clip = track;
+        _audioSource.Play();
+    }
+
+    void Update()
+    {
+        float[] spectrumData = _audioSource.GetSpectrumData(visualizerSimples, 0, FFTWindow.Rectangular);
+
+        for (int i = 0; i < visualizerObjects.Length; i++)
+        {
+            Vector2 newSize = visualizerObjects[i].GetComponent<RectTransform>().rect.size;
+
+            newSize.y = Mathf.Lerp(newSize.y, minHeight + (spectrumData[i] * (maxHeight - minHeight) * 5.0f), updateSenstivity);
+            visualizerObjects[i].GetComponent<RectTransform>().sizeDelta = newSize;
+        }
+    }
+}

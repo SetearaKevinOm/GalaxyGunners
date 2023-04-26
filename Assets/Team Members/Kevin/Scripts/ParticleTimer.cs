0b3f983260dfe3090bf8c98e5c982ceb4dcc30b8
SetearaKevinOm <1024325@student.sae.edu.au>
2023-04-27 00:12:20 +1000

    1

---
+++ Assets/Team Members/Kevin/Scripts/ParticleTimer.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ParticleTimer : MonoBehaviour
+{
+    public IEnumerator Start()
+    {
+        yield return new WaitForSeconds(15f);
+        Destroy(gameObject);
+    }
+}

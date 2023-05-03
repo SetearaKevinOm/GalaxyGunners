6151c20b40f9110ed9e5607757733ec6df9f5997
Bryce Sandall <1024240@student.sae.edu.au>
2023-05-03 22:24:57 +1000

    Bloodhawk Fly in sound

---
+++ Assets/Team Members/Bryce/Audio/SFX/Bloodhawk_SFX_Script.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Bloodhawk_SFX_Script : MonoBehaviour
+{
+	public AudioSource bloodhawkSFX;
+
+	public void PlayFlyBy()
+	{
+		bloodhawkSFX.Play();
+	}
+}
